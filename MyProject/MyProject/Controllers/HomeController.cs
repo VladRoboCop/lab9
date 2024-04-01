9d5dfe70588218cfda3ea300dea3659f029988e3
VladRoboCop <113367165+VladRoboCop@users.noreply.github.com>
2024-04-01 18:32:22 +0300

    Add files via upload

---
+++ MyProject/MyProject/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var objects = new List<MyObject>
+        {
+            new MyObject { ID = 1, Name = "Object 1", Price = 10 },
+            new MyObject { ID = 2, Name = "Object 2", Price = 20 },
+            new MyObject { ID = 3, Name = "Object 3", Price = 30 }
+        };
+
+        return View(objects);
+    }
+}
---
+++ MyProject/MyProject/Models/MyObject.cs
@@ -0,0 +1,7 @@
+﻿// MyObject.cs
+public class MyObject
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
